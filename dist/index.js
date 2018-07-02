aa957c1d3abf30b723fe20bdc36d1202d37d7617
Niwatori <oniwatori1102@gmail.com>
2018-07-02 07:23:26 +0700

    JS6 demo

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var Startup = /** @class */ (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        var name = "Name";
+        var message = name + " have a nice day!\n        asdasadsa";
+        console.log(message);
+    };
+    return Startup;
+}());
+Startup.main();
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,9 @@
+class Startup {
+    public static main(): void {
+        var name = "Name";
+        var message = `${name} have a nice day!
+        asdasadsa`;
+        console.log(message);
+    }
+}
+Startup.main();
